b73e7e293e998ee5eab9e2b7550b26aa412ce2d5
Anton <132255178+dxfh00@users.noreply.github.com>
2024-09-22 11:14:52 +0300

    module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,21 +1,23 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
-        self.x_distance = dx
-
-
+        self.x_distance += dx
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
-        self.y_distance = dy
-
+        self.y_distance += dy
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         Horse.run(self, dx)
@@ -25,8 +27,7 @@ class Pegasus(Horse, Eagle):
         return (self.x_distance, self.y_distance)
 
     def voice(self):
-        print(Eagle.sound)
-
+        print(self.sound)
 
 p1 = Pegasus()
 
@@ -35,4 +36,5 @@ p1.move(10, 15)
 print(p1.get_pos())
 p1.move(-5, 20)
 print(p1.get_pos())
+
 p1.voice()
